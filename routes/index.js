c895b9f9c2f4aaf88f73fd6871299b68462c7966
Protimus <rafaelgorges@outlook.com>
2020-01-30 15:34:19 -0300

    Created endpoint to authenticate endpoints with JWT

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express'),
+jwt = require('jwt-simple'),
+moment = require('moment'),
+config = require('config'),
+router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Node REST API using SQL created by Rafael P C Gorges (Protimus)' });
+});
+
+/* POST Login Auth with JWT */
+router.post('/login', function(request, response, next){
+  var username = request.body.username;
+  var password = request.body.password;
+  if(username === 'protimus' && password === '123456'){
+    var expires = moment().add(7, 'days').valueOf();
+    var token = jwt.encode({
+      user: username,
+      exp: expires
+    },
+    config.get('jwtTokenSecret'));
+    response.json({
+      token: token
+    });
+  } else {
+    var err = new Error('Unathorized');
+    err.status = 401;
+    next(err);
+  }
 });
 
 module.exports = router;
