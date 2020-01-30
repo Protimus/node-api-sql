27f43cfc2c0f6b648e3e826cb4ce7b9c42840358
Protimus <rafaelgorges@outlook.com>
2020-01-30 15:47:49 -0300

    Created JWT authentication middleware for users endpoints

--- routes/users.js
+++ routes/users.js
@@ -1,26 +1,51 @@
 var express = require('express');
 var user = require("../controllers/user.controller.js");
 var router = express.Router();
+var jwt = require('jwt-simple'),
+config = require('config');
+
+var auth = function(request, response, next){
+    var token = request.query.token;
+    if(!token){
+        var err = new Error('Forbidden');
+        err.status = 403;
+        return next(err);
+    }
+    try {
+        var decoded = jwt.decode(token, config.get('jwtTokenSecret'));
+        var isExpired = moment(decoded.exp).isBefore(new Date());
+        if(isExpired){
+            var err = new Error('Unauthorized');
+            err.status = 401;
+            return next(err);
+        } else {
+            request.user = decoded.user;
+            next();
+        }
+    } catch(err){
+        return next(err);
+    }
+};
 
 // Create a new user
-router.post("/", user.create);
+router.post("/", auth, user.create);
 
 // Retrieve all users
-router.get("/", user.findAll);
+router.get("/", auth, user.findAll);
 
 // Retrieve all active users
-router.get("/active", user.findAllActive);
+router.get("/active", auth, user.findAllActive);
 
 // Retrieve a single user with id
-router.get("/:id", user.findOne);
+router.get("/:id", auth, user.findOne);
 
 // Update a user with id
-router.put("/:id", user.update);
+router.put("/:id", auth, user.update);
 
 // Delete a user with id
-router.delete("/:id", user.delete);
+router.delete("/:id", auth, user.delete);
 
 // Delete all users
-router.delete("/", user.deleteAll);
+router.delete("/", auth, user.deleteAll);
 
 module.exports = router;
